2dcb0390eed17c7c94f63fe96a0f03d2b8b181ac
hazemmnaser <139693389+hazemmnaser@users.noreply.github.com>
2023-07-28 19:04:34 +0300

    Update Dev_calc.py

--- Dev_calc.py
+++ Dev_calc.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2023 hazemmnaser 
- # All rights reserved 
- # By depend on free software rights, you have freedom to do anything; 
- # However, you must include the copyright of the original software owner. 
- # This project is licensed under the GPLv2 license, for more informations, 
- # read the <<LICENSE>> file 
+# All rights reserved 
+# By depend on free software rights, you have freedom to do anything; 
+# However, you must include the copyright of the original software owner. 
+# This project is licensed under the GPLv2 license, for more informations, 
+# read the <<LICENSE>> file 
  
 from tkinter import *
 from random import random
